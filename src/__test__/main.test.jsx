ac884a9153e612a733cecbfe4111c99613fe9f99
Neul Seo <neul.seo.1213@gmail.com>
2024-04-22 17:56:16 -0400

    created server.test.jsx

---
+++ src/__test__/main.test.jsx
@@ -0,0 +1,5 @@
+// testing for main (frontend)
+// requirements:
+// - the page should render
+// - upon clicking any of the three buttons at the bottom, new movie should display
+// - profile + movie archive tabs 
