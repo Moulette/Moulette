3343ac29e41fa4ce126b097ec5c9b077fb96a994
Evan Griffith <evan@evangriffith.net>
2024-04-22 14:01:25 -0700

    Basic navbar functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
+import avatar from '../assets/avatar.svg';
+import xMark from '../assets/xMark.svg';
 
-const NavBar = ({profileDisplayed, setProfileDisplayed, setLoggedIn}) => {
-
-
-
-
+const NavBar = ({ profileDisplayed, setProfileDisplayed, setLoggedIn }) => {
   return (
-    <div className="flex">
-      <div>
-        X
-      </div>
+    <div className="flex justify-between items-center m-2 mx-10">
+      <div className=""></div>
       <p>Moulette</p>
-      <div>
-        Profile
+      <div onClick={() => setProfileDisplayed(!profileDisplayed)}>
+        <img
+          className="h-6 text-grey-500"
+          src={profileDisplayed ? xMark : avatar}
+        />
       </div>
     </div>
   );
