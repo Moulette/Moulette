2184bf0b248b55771c4b62f963d9ceb94f5a3b1b
Evan Griffith <evan@evangriffith.net>
2024-04-22 23:02:34 -0700

    Adjusted sample user to be Ryan

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,10 +1,13 @@
 const UserHeader = () => {
   return (
     <div className="flex min-w-full flex-row justify-start rounded-3xl bg-stone-200 p-5 shadow-lg">
-      <img className="rounded-full" src="https://placehold.co/100x100" />
+      <img
+        className="h-28 w-28 rounded-full"
+        src="https://media.licdn.com/dms/image/C5603AQH89aF7fDuxYQ/profile-displayphoto-shrink_800_800/0/1661982850851?e=1719446400&v=beta&t=3IH842Pn3N08WAz_LuQC5-X-JQtlfZxolMdA_C4Uu8M"
+      />
       <div className="ml-5 flex flex-col items-start justify-start">
-        <h3 className="font-bold">Joe Schmoe</h3>
-        <p>Los Angeles, CA</p>
+        <h3 className="font-bold">Ryan McDaniel</h3>
+        <p>New York, NY</p>
       </div>
     </div>
   );
