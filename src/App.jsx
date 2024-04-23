ce1ee9718c2e0ae75589172fa97440ff018fb199
Evan Griffith <evan@evangriffith.net>
2024-04-23 00:04:09 -0700

    Finished basic hardcoded login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import { useState } from "react";
 import NavBar from "./components/NavBar";
 import Profile from "./containers/Profile";
 import RatingPage from "./containers/RatingPage";
+import Login from "./components/Login";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(true);
+  const [currentUser, setCurrentUser] = useState(null);
   const [profileDisplayed, setProfileDisplayed] = useState(false);
+
+  let renderedComponent = <Login setCurrentUser={setCurrentUser} />;
+
+  if(currentUser !== null) {
+    renderedComponent = profileDisplayed ? <Profile /> : <RatingPage />;
+  }
 
   return (
     <div className="flex flex-col h-screen bg-zinc-100 overflow-y-auto">
@@ -13,11 +20,11 @@ function App() {
         <NavBar
           profileDisplayed={profileDisplayed}
           setProfileDisplayed={setProfileDisplayed}
-          loggedIn={loggedIn}
+          currentUser={currentUser}
         />
       </div>
-      <div className="flex-1">
-        {profileDisplayed ? <Profile /> : <RatingPage />}
+      <div className="flex-1 flex flex-col justify-center">
+        {renderedComponent}
       </div>
     </div>
   );
