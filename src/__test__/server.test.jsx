0ba7f97bcf487c83903032bdbf47c0b1dc02555e
Neul Seo <neul.seo.1213@gmail.com>
2024-04-23 17:30:43 -0400

    added testing for listController, getWatchMovieList

--- src/__test__/server.test.jsx
+++ src/__test__/server.test.jsx
@@ -7,12 +7,73 @@
 // - external dependencies (ex. tailwind)
 
 const request = require("supertest");
-const app = require("../app");
+const express = require("express");
+const listRoutes = require("../../server/routes/list");
+const listController = require("../../server/controllers/listController");
+const pool = require("../../server/db");
+const { list } = require("postcss");
 
-describe("GET /users", () => {
-  test("should return all users", async () => {
-    const response = await request(app).get("/users");
-    expect(response.status).toBe(200);
+const app = express();
+app.use(express.json());
+app.use("/list", listRoutes);
+
+// mocking pool.query
+jest.mock("../../server/db");
+
+describe("listController", () => {
+  describe("GET /list", () => {
+    it("should return status 200 and all watch list movies", async () => {
+      const mockRows = [
+        { id: 1, title: "movie1" },
+        { id: 2, title: "movie2" },
+      ];
+      listController.getAllWishListedMovies = jest
+        .fn()
+        .mockResolvedValue(mockRows);
+
+      // Make HTTP GET request to the route
+      const response = await request(app).get("/api/wishlist");
+
+      // Assert response
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(mockRows);
+    });
+  });
+
+  describe("getWatchMovieList", () => {
+    it("should return all movies in the Watch Movie list", async () => {
+      const mockRows = [
+        { id: 1, title: "movie1" },
+        { id: 2, title: "movie2" },
+      ];
+      pool.query.mockResolvedValue({ rows: mockRows });
+
+      const req = {};
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const next = jest.fn();
+
+      await listController.getAllWishListedMovies(req, res, next);
+
+      expect(pool.query).toHaveBeenCalledWith("SELECT * FROM watch_list");
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith(mockRows);
+      expect(next).not.toHaveBeenCalledWith();
+    });
+
+    it("should call next with error, if query fails", async () => {
+      const error = new Error("Database error");
+      pool.query.mockRejectedValue(error);
+
+      const req = {};
+      const res = {};
+      const next = jest.fn();
+
+      await listController.getAllWishListedMovies(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
   });
 });
-npm 
