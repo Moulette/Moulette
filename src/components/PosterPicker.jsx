f34f474041dfa383fe15df46289da4b254bf0c62
Evan Griffith <evan@evangriffith.net>
2024-04-23 16:24:59 -0700

    Installed framerMotion, basic dragging functionality with opacity transition

--- src/components/PosterPicker.jsx
+++ src/components/PosterPicker.jsx
@@ -1,12 +1,29 @@
 import PosterImage from "./PosterImage";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
-const PosterPicker = ({ currentMovies }) => {
+/**
+ * Component that displays the topmost two PosterImages, and contains the logic for swiping/animating moving the images on the page.
+ * @returns
+ */
+const PosterPicker = ({
+  currentMovies,
+  handleDontWatch,
+  handleWatched,
+  handleAddWatchList,
+}) => {
+
+  const x = useMotionValue(0);
+  // If dragged more than 150px in any direction, fade to 0 in the next 50px
+  const opacity = useTransform(x, [-150, 0, 150], [0, 1, 0]);
+
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="relative">
-          <PosterImage movie={currentMovies[0]} top={true} />
+        <PosterImage movie={currentMovies[0]} top={true} />
         <div className="absolute top-0">
-          <PosterImage movie={currentMovies[1]} />
+          <motion.div drag="x" dragConstraints={{ left: -300, right: 300 }} style={{x, opacity}} >
+            <PosterImage movie={currentMovies[1]} />
+          </motion.div>
         </div>
       </div>
     </div>
