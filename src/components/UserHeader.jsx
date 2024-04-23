91df8fe86432b7cc37a77c52b7449b9bc3755b3f
Evan Griffith <evan@evangriffith.net>
2024-04-22 22:49:20 -0700

    Added shadow to user header and movie card items

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,6 +1,6 @@
 const UserHeader = () => {
   return (
-    <div className="flex min-w-full flex-row justify-start rounded-3xl bg-stone-200 p-5">
+    <div className="flex min-w-full flex-row justify-start rounded-3xl bg-stone-200 p-5 shadow-lg">
       <img className="rounded-full" src="https://placehold.co/100x100" />
       <div className="ml-5 flex flex-col items-start justify-start">
         <h3 className="font-bold">Joe Schmoe</h3>
